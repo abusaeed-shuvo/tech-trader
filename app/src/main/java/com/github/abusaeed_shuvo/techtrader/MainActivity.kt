2f1e71e17103d2a5ffa5deed6e220e87a6f0a4a1
abusaeed-shuvo <96466655+abusaeed-shuvo@users.noreply.github.com>
2025-07-05 01:12:24 +0600

    incremental update

--- app/src/main/java/com/github/abusaeed_shuvo/techtrader/MainActivity.kt
+++ app/src/main/java/com/github/abusaeed_shuvo/techtrader/MainActivity.kt
@@ -5,14 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.abusaeed_shuvo.techtrader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+	private lateinit var binding: ActivityMainBinding
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		binding = ActivityMainBinding.inflate(layoutInflater)
 		enableEdgeToEdge()
-		setContentView(R.layout.activity_main)
-		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+		setContentView(binding.root)
+		ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+			val systemBars =
+				insets.getInsets(
+					WindowInsetsCompat.Type.systemBars()
+							or WindowInsetsCompat.Type.ime()
+							or WindowInsetsCompat.Type.displayCutout()
+				)
 			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 			insets
 		}
